da4fb22b7ba015cef3cd01652bc4f12aabe7bea9
LuK <lucas.tladi@outlook.com>
2024-11-13 13:43:05 +0200

    latest

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* ================================== typing animation ================================== */
+var typed = new Typed(".typing", 
+{
+    strings:["","Application Developer", "Web Developer", "Back-End Developer", "Security Administrator"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
