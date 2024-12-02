71fb225aa0539b9c67f6c97e79d6fe3d0293988c
LuK <lucas.tladi@outlook.com>
2024-12-02 15:59:31 +0200

    latest
    Incorporated the dark mode feature into the chatbot. Made the message input box bigger.

--- js/script.js
+++ js/script.js
@@ -319,3 +319,7 @@ const nav = document.querySelector(".nav"),
             chatbotToggler.addEventListener("click", () => document.body.classList.toggle("show-chatbot"));
 
             closeChatbot.addEventListener("click", () => document.body.classList.remove("show-chatbot"));
+
+
+            
+        
